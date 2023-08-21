180fd89ba8cc2c31fa1f0054fe7245e383a31381
Maria Hola <maaria.hola@gmail.com>
2023-08-21 23:01:48 +0200

    changes with immutability policy

--- js/script.js
+++ js/script.js
@@ -1,22 +1,32 @@
 {
 
-    const tasks = [];
+    let tasks = [];
     let hideDoneTasks = false;
 
     const removeTask = (taskIndex) => {
-        tasks.splice(taskIndex, 1);
+        tasks = [
+            ...tasks.slice(0, taskIndex),
+            ...tasks.slice(taskIndex + 1),
+        ];
         render();
-    }
+    };
 
     const toggleTaskDone = (taskIndex) => {
-        tasks[taskIndex].done = !tasks[taskIndex].done;
+        tasks = [
+            ...tasks.slice(0, taskIndex),
+            {
+                ...tasks[taskIndex],
+                done: !tasks[taskIndex].done,
+            },
+            ...tasks.slice(taskIndex + 1),
+        ];
         render();
-    }
+    };
 
     const addNewTask = (newTaskContent) => {
         tasks = [
             ...tasks,
-            { content: newTaskContent },
+            { content: newTaskContent }
         ];
         render();
     };
